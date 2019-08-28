ded5f780999cfa816efcac902a7f875bb54e9944
MoriyaShiine <moriyashiine@gmail.com>
2019-08-28 09:30:11 -0500

    lol

--- src/main/java/moriyashiine/crashma/Crashma.java
+++ src/main/java/moriyashiine/crashma/Crashma.java
@@ -1,8 +1,13 @@
 package moriyashiine.crashma;
 
+import net.minecraft.crash.CrashReport;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Mod(modid = Crashma.MODID, name = Crashma.NAME, version = Crashma.VERSION)
 public class Crashma {
@@ -14,13 +19,13 @@ public class Crashma {
         MinecraftForge.EVENT_BUS.register(this);
     }
     
-    //    @SubscribeEvent
-    //    public void lol(TickEvent.WorldTickEvent event)
-    //    {
-    //        if (!event.world.isRemote)
-    //        {
-    //            long time = world.getTotalWorldTime();
-    //            if (time > 0 && time % (60*20) == 0 && event.world.rand.nextInt(3) == 0) throw new CrashReport();
-    //        }
-    //    }
+    @SubscribeEvent
+    public void lol(TickEvent.WorldTickEvent event)
+    {
+        if (!event.world.isRemote)
+        {
+            long time = event.world.getTotalWorldTime();
+            if (time > 0 && time % (60*20) == 0 && event.world.rand.nextInt(3) == 0) FMLCommonHandler.instance().raiseException(new Throwable(), "gottem", true);
+        }
+    }
 }
