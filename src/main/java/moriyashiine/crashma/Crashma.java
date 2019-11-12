dd319560b917e8b856186c0bb374fac74f0115fd
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2019-11-11 20:33:17 -0600

    Add files via upload

--- src/main/java/moriyashiine/crashma/Crashma.java
+++ src/main/java/moriyashiine/crashma/Crashma.java
@@ -1,31 +1,34 @@
 package moriyashiine.crashma;
 
-import net.minecraft.crash.CrashReport;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(modid = Crashma.MODID, name = Crashma.NAME, version = Crashma.VERSION)
+@Mod("crashma")
 public class Crashma {
-    static final String MODID = "crashma", NAME = "Crashma", VERSION = "69";
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-    
-    @SubscribeEvent
-    public void lol(TickEvent.WorldTickEvent event)
-    {
-        if (!event.world.isRemote)
-        {
-            long time = event.world.getTotalWorldTime();
-            if (time > 0 && time % (60*20) == 0 && event.world.rand.nextInt(3) == 0) FMLCommonHandler.instance().raiseException(new Throwable(), "gottem", true);
-        }
-    }
+	public Crashma() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+	}
+	
+	private void setup(final FMLCommonSetupEvent event) {
+		MinecraftForge.EVENT_BUS.register(new Handler());
+	}
+	
+	private static class Handler {
+		@SuppressWarnings("ConstantConditions")
+		@SubscribeEvent
+		public void worldTick(TickEvent.WorldTickEvent event) {
+			World world = event.world;
+			if (!world.isRemote)
+			{
+				long time = world.getGameTime();
+				if (time > 0 && time % 1200 == 0 && world.rand.nextInt(3) == 0) ((Entity) null).isBurning();
+			}
+		}
+	}
 }
