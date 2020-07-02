9dd3095b1f43d233e147a3360052bd051ec6c517
MoriyaShiine <moriyashiine@gmail.com>
2020-07-01 20:19:38 -0500

    update to 1.16 and move to fabric

--- src/main/java/moriyashiine/crashma/Crashma.java
+++ src/main/java/moriyashiine/crashma/Crashma.java
@@ -1,34 +1,23 @@
 package moriyashiine.crashma;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.world.WorldTickCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("crashma")
-public class Crashma {
-	public Crashma() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+public class Crashma implements ModInitializer, WorldTickCallback {
+	@Override
+	public void onInitialize() {
+		WorldTickCallback.EVENT.register(this);
 	}
 	
-	private void setup(final FMLCommonSetupEvent event) {
-		MinecraftForge.EVENT_BUS.register(new Handler());
-	}
-	
-	private static class Handler {
-		@SuppressWarnings("ConstantConditions")
-		@SubscribeEvent
-		public void worldTick(TickEvent.WorldTickEvent event) {
-			World world = event.world;
-			if (!world.isRemote)
-			{
-				long time = world.getGameTime();
-				if (time > 0 && time % 1200 == 0 && world.rand.nextInt(3) == 0) ((Entity) null).isBurning();
-			}
+	@SuppressWarnings("ConstantConditions")
+	@Override
+	public void tick(World world) {
+		if (!world.isClient)
+		{
+			long time = world.getTime();
+			if (time > 0 && time % 1200 == 0 && world.random.nextInt(3) == 0) ((Entity) null).isOnFire();
 		}
 	}
 }
