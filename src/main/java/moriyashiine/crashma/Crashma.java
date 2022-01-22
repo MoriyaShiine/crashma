9918ab6c75650b6b1529a6e9f9d39603cd517ca0
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-01-22 09:19:09 -0600

    update dev to 1.18 (license pending?)

--- src/main/java/moriyashiine/crashma/Crashma.java
+++ src/main/java/moriyashiine/crashma/Crashma.java
@@ -4,21 +4,17 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.world.WorldTickCallback;
 import net.minecraft.util.crash.CrashException;
 import net.minecraft.util.crash.CrashReport;
-import net.minecraft.world.World;
 
-public class Crashma implements ModInitializer, WorldTickCallback {
+public class Crashma implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		WorldTickCallback.EVENT.register(this);
-	}
-	
-	@Override
-	public void tick(World world) {
-		if (!world.isClient()) {
-			long time = world.getTime();
-			if (time > 0 && time % 1200 == 0 && world.getRandom().nextInt(3) == 0) {
-				throw new CrashException(new CrashReport("crashma game lmao idiot gottem", new DumbassException()));
+		WorldTickCallback.EVENT.register(world -> {
+			if (!world.isClient()) {
+				long time = world.getTime();
+				if (time > 0 && time % 1200 == 0 && world.getRandom().nextInt(3) == 0) {
+					throw new CrashException(new CrashReport("crashma game lmao idiot gottem", new DumbassException()));
+				}
 			}
-		}
+		});
 	}
 }
