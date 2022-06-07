630f60615fff5cb9efe0176960164888f35048b4
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-06-07 16:12:51 -0500

    update to 1.19

--- src/main/java/moriyashiine/crashma/client/CrashmaClient.java
+++ src/main/java/moriyashiine/crashma/client/CrashmaClient.java
@@ -1,17 +1,15 @@
 package moriyashiine.crashma.client;
 
-import moriyashiine.crashma.client.integration.botania.ModBotaniaClientIntegration;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.loader.api.FabricLoader;
 
 @Environment(EnvType.CLIENT)
 public class CrashmaClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		if (FabricLoader.getInstance().isModLoaded("botania")) {
-			ModBotaniaClientIntegration.init();
-		}
+//		if (FabricLoader.getInstance().isModLoaded("botania")) {
+//			ModBotaniaClientIntegration.init();
+//		}
 	}
 }
--- src/main/java/moriyashiine/crashma/common/Crashma.java
+++ src/main/java/moriyashiine/crashma/common/Crashma.java
@@ -1,28 +1,26 @@
 package moriyashiine.crashma.common;
 
-import moriyashiine.crashma.common.integration.botania.ModBotaniaIntegration;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.world.WorldTickCallback;
-import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.CrashReport;
 import net.minecraft.ReportedException;
+import net.minecraft.world.level.Level;
 
 public class Crashma implements ModInitializer {
 	public static final String MOD_ID = "crashma";
 
 	@Override
 	public void onInitialize() {
-		WorldTickCallback.EVENT.register(level -> {
-			if (!level.isClientSide()) {
-				long time = level.getGameTime();
-				if (time > 0 && time % 1200 == 0 && level.getRandom().nextInt(3) == 0) {
-					crashma();
-				}
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			Level overworld = server.overworld();
+			long time = overworld.getGameTime();
+			if (time > 0 && time % 1200 == 0 && overworld.getRandom().nextInt(3) == 0) {
+				crashma();
 			}
 		});
-		if (FabricLoader.getInstance().isModLoaded("botania")) {
-			ModBotaniaIntegration.init();
-		}
+//		if (FabricLoader.getInstance().isModLoaded("botania")) {
+//			ModBotaniaIntegration.init();
+//		}
 	}
 
 	public static void crashma() {
